e7f6c4bd3a978c1c6f6cb1ad524df1830dc897c8
Mattia Ryffel <mattia.ryffel@disneyresearch.com>
2017-02-21 15:18:41 +0100

    replaced AudioCallBacks with UnityEvents

--- Example.cs
+++ Example.cs
@@ -17,45 +17,36 @@
 using UnityEngine;
 using System;
 
-/*
- * Make your class implement the interface AudioProcessor.AudioCallbaks
- */
-public class Example : MonoBehaviour, AudioProcessor.AudioCallbacks
+public class Example : MonoBehaviour
 {
     
-    void Start()
-    {
-        //Select the instance of AudioProcessor and pass a reference
-        //to this object
-        AudioProcessor processor = FindObjectOfType<AudioProcessor>();
-        processor.addAudioCallback(this);
-    }
+	void Start ()
+	{
+		//Select the instance of AudioProcessor and pass a reference
+		//to this object
+		AudioProcessor processor = FindObjectOfType<AudioProcessor> ();
+		processor.onBeat.AddListener (onOnbeatDetected);
+		processor.onSpectrum.AddListener (onSpectrum);
+	}
 
-    
-    void Update()
-    {
-        
-    }
+	//this event will be called every time a beat is detected.
+	//Change the threshold parameter in the inspector
+	//to adjust the sensitivity
+	void onOnbeatDetected ()
+	{
+		Debug.Log ("Beat!!!");
+	}
 
-    //this event will be called every time a beat is detected.
-    //Change the threshold parameter in the inspector
-    //to adjust the sensitivity
-    public void onOnbeatDetected()
-    {
-        Debug.Log("Beat!!!");
-    }
+	//This event will be called every frame while music is playing
+	void onSpectrum (float[] spectrum)
+	{
+		//The spectrum is logarithmically averaged
+		//to 12 bands
 
-    //This event will be called every frame while music is playing
-    public void onSpectrum(float[] spectrum)
-    {
-        //The spectrum is logarithmically averaged
-        //to 12 bands
-
-        for (int i = 0; i < spectrum.Length; ++i)
-        {
-            Vector3 start = new Vector3(i, 0, 0);
-            Vector3 end = new Vector3(i, spectrum[i], 0);
-            Debug.DrawLine(start, end);
-        }
-    }
+		for (int i = 0; i < spectrum.Length; ++i) {
+			Vector3 start = new Vector3 (i, 0, 0);
+			Vector3 end = new Vector3 (i, spectrum [i], 0);
+			Debug.DrawLine (start, end);
+		}
+	}
 }
